226940d5416a6f7876bfb337592ef8ff9d1aa687
mk-isos <mkisos7900@naver.com>
2024-07-06 16:50:59 +0900

    240706 Update 1267.cpp

--- Baekjoon Online Judge/0x02강 - 기초 코드 작성 요령 II/1267.cpp
+++ Baekjoon Online Judge/0x02강 - 기초 코드 작성 요령 II/1267.cpp
@@ -10,13 +10,12 @@ int main(){
     int N;
     cin >> N;
 
-    for(int i = 0; i < N; i++) cin >> phone[n];
+    for(int i = 0; i < N; i++) cin >> phone[i];
 
-    for(int i = 0; i < N; i++) Y += ((phone[n] / 30) + 1) * 10;
-    for(int i = 0; i < N; i++) M += ((phone[n] / 60) + 1) * 15;
+    for(int i = 0; i < N; i++) Y += ((phone[i] / 30) + 1) * 10;
+    for(int i = 0; i < N; i++) M += ((phone[i] / 60) + 1) * 15;
 
-    if(Y > M) cout << "Y " << Y;
+    if(Y < M) cout << "Y " << Y;
     else if(Y > M) cout << "M " << M;
     else cout << "Y M " << Y;
 }
-
