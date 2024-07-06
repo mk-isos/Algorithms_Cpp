4cb6a7248f10c830479afbf97946589f65d1cf2c
mk-isos <mkisos7900@naver.com>
2024-07-06 19:19:54 +0900

    240706 Update 0x03.cpp [0x01강 연습문제 다른 풀이]

--- Baekjoon Online Judge/0x03강-배열/0x03.cpp
+++ Baekjoon Online Judge/0x03강-배열/0x03.cpp
@@ -13,3 +13,14 @@
 
 //vector
 
+//0x01강 연습문제
+
+int func2(int arr[], int N){
+    int occur[101] = {};
+    for(int i = 0; i < N; i++){
+        if(occur[100-arr[i]] == 1)
+        return 1;
+        occur[arr[i]] = 1;
+    }
+    return 0;
+}
