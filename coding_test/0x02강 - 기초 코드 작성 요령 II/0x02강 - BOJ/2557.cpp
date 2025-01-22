62d1a55f48df3e7dfb26eb34bfee4ec32c8963b1
mk-isos <mkisos7900@naver.com>
2025-01-22 09:20:53 +0900

    Update .gitignore rules

--- coding_test/0x02강 - 기초 코드 작성 요령 II/0x02강 - BOJ/2557.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cout << "Hello World!";
-}
--- coding_test/BOJ-강의x/23234.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "The world says hello!" << endl;
-    return 0;
-}
