671e775fcb2934d21f67e6abdac412a74f29cac9
mk-isos <mkisos7900@naver.com>
2025-01-23 11:12:40 +0900

    250123 Update BFS

--- coding_test/0x09강 - BFS/BFS_P1.cpp
+++ coding_test/0x09강 - BFS/BFS_P1.cpp
@@ -17,23 +17,37 @@ bool vis[502][502]; // 해당 칸을 방문했는지 여부를 저장
 
 int n = 7, m = 10; // n = 행의 수, m = 열의 수
 
-//dx,dy 
-
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1}; // 상하좌우 네 방향을 의미
 
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    queue<pair<int,int>> Q;
+    queue<pair<int, int>> Q;
 
     vis[0][0] = 1;
-    Q.push({0,0});
+    Q.push({0, 0});
 
-    while(!Q.empty()) {
-        pair<int,int> cur = Q.front; Q.pop;
+    while (!Q.empty())
+    {
+        pair<int, int> cur = Q.front(); Q.pop();
         cout << '(' << cur.X << ", " << cur.Y << ") -> ";
 
-        
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+
+            if (vis[nx][ny] || board[nx][ny] != 1)
+                continue;
+
+            vis[nx][ny] = 1;
+            Q.push({nx, ny});
+        }
     }
 }
