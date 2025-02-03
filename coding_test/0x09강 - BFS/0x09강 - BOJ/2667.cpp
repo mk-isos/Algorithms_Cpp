37665e920a356c7691b9856c635df9f7aac30d0c
mk-isos <mkisos7900@naver.com>
2025-02-03 10:31:08 +0900

    250203 Create 2667.cpp

---
+++ coding_test/0x09강 - BFS/0x09강 - BOJ/2667.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+#define X first
+#define Y second
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int n;
+string board[27];
+int vis[27][27];
+
+int main()
+{
+    FIO;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> board[i];
+    }
+
+    int count = 0;
+    vector<int> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (board[i][j] == '0' || vis[i][j] == 1)
+                continue;
+
+            queue<pair<int, int>> Q;
+            vis[i][j] = 1;
+            Q.push({i, j});
+            int width = 1;
+            count++;
+
+            while (!Q.empty())
+            {
+                auto cur = Q.front();
+                Q.pop();
+                for (int dir = 0; dir < 4; dir++)
+                {
+                    int nx = cur.X + dx[dir];
+                    int ny = cur.Y + dy[dir];
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                        continue;
+                    if (board[nx][ny] == '0' || vis[nx][ny] == 1)
+                        continue;
+                    Q.push({nx, ny});
+                    vis[nx][ny] = 1;
+                    width++;
+                }
+            }
+            ans.push_back(width);
+        }
+    }
+
+    cout << count << '\n';
+    sort(ans.begin(), ans.end());
+
+    for (int i : ans)
+        cout << i << '\n';
+
+    return 0;
+}
