19644c6f3907a304dd23a955b92eda85404b9a52
mk-isos <mkisos7900@naver.com>
2025-02-09 17:30:31 +0900

    250209 Create 9. Pailndrome Number.cpp

---
+++ leetcode/9. Palindrome Number.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        long long reversed = 0;
+        int original = x;
+
+        while (x > 0)
+        {
+            reversed = reversed * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    int x;
+
+    cin >> x;
+
+    if (sol.isPalindrome(x))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
