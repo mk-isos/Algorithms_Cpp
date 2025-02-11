b1d38777c72a769611f706ac736ec5c379f605bb
mk-isos <mkisos7900@naver.com>
2025-02-11 16:02:32 +0900

    250211 Create 13319.cpp

---
+++ coding_test/BOJ-강의x/13319_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    // n = 1299963601, m = 601 (n = 601 * 1201 * 1801)
+    cout << 1299963601LL << " " << 601 << "\n";
+    return 0;
+}
