321717efd2b1048c34d2f974c329226e0380d3f7
mk-isos <mkisos7900@naver.com>
2025-02-11 16:01:51 +0900

    250211 Create 13319.cpp

---
+++ coding_test/BOJ-강의x/13319.cpp
@@ -0,0 +1,31 @@
+// 카마이클 수
+//  카마이클 수는 페르마 소정리를 모든 밑에 대해 만족하지만 합성수인 수
+
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    // 반례: n = 54519328481, 소인수 m = 503
+    cout << "54519328481 503";
+    return 0;
+}
+
+// 아래도 답
+/*
+
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main(){
+    FIO;
+    // n = 1299963601, m = 601 (n = 601 * 1201 * 1801)
+    cout << 1299963601LL << " " << 601 << "\n";
+    return 0;
+}
+
+
+*/
