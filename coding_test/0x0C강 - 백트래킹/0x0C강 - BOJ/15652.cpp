b48d333ef2e53c32246a98ff9afc83d6c976bd03
mk-isos <mkisos7900@naver.com>
2025-02-13 10:31:47 +0900

    250213 Create 15652.cpp

---
+++ coding_test/0x0C강 - 백트래킹/0x0C강 - BOJ/15652.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int N, M;
+vector<int> seq;
+
+void backtrack(int start)
+{
+    if (seq.size() == M)
+    {
+        for (int num : seq)
+        {
+            cout << num << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i <= N; i++)
+    {
+        seq.push_back(i);
+        backtrack(i);
+        seq.pop_back();
+    }
+}
+
+int main()
+{
+    FIO;
+    cin >> N >> M;
+    backtrack(1);
+    return 0;
+}
