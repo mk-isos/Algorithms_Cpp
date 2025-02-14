1ffb2a373472ea0d31c5616ed45ac9bf446d0298
mk-isos <mkisos7900@naver.com>
2025-02-14 09:17:39 +0900

    250214 Update 15655.cpp

--- coding_test/0x0C강 - 백트래킹/0x0C강 - BOJ/15655.cpp
+++ coding_test/0x0C강 - 백트래킹/0x0C강 - BOJ/15655.cpp
@@ -5,27 +5,33 @@ using namespace std;
 int N, M;
 vector<int> num, seq;
 
-void backtrack(int start) {
-    if(seq.size()==M) {
-        for(int num : seq) cout << num << " ";
+void backtrack(int start)
+{
+    if (seq.size() == M)
+    {
+        for (int num : seq)
+            cout << num << " ";
         cout << "\n";
         return;
     }
 
-    for(int i = start; i < N; i++) {
+    for (int i = start; i < N; i++)
+    { // start 이후의 숫자만 선택 → 중복 방지.
         seq.push_back(num[i]);
-        backtrack(i+1);
+        backtrack(i + 1); // 중복되지 않게 다음 숫자부터 선택
         seq.pop_back();
     }
 }
 
-int main(){
+int main()
+{
     FIO;
 
     cin >> N >> M;
     num.resize(N);
 
-    for(int i = 0; i < N; i++) cin >> num[i];
+    for (int i = 0; i < N; i++)
+        cin >> num[i];
 
     sort(num.begin(), num.end());
 
