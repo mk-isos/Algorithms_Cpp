ffcf262788b411f75b934c50092acad2886a981a
mk-isos <mkisos7900@naver.com>
2025-02-16 03:14:52 +0900

    250215 Create 1003.cpp

---
+++ coding_test/BOJ-강의x/1003.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+long long fiboarr[50] = {
+    0,
+    1,
+};
+long long fibo(int N)
+{
+    if (N == 0 || N == 1)
+        return fiboarr[N];
+    else if (fiboarr[N] == 0)
+        fiboarr[N] = fibo(N - 1) + fibo(N - 2);
+    return fiboarr[N];
+}
+int main()
+{
+
+    FIO;
+    int T;
+    cin >> T;
+    int tmp;
+    for (int i = 0; i < T; i++)
+    {
+        cin >> tmp;
+        if (tmp == 0)
+            cout << "1 0" << '\n';
+        else
+            cout << fibo(tmp - 1) << ' ' << fibo(tmp) << '\n';
+    }
+}
