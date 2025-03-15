baa8099832f5b8c43b9d6a054c35ea93a526e6d2
mk-isos <mkisos7900@naver.com>
2025-03-15 09:05:09 +0900

    250315 Update 21.merge-two-sorted-lists.cpp

--- leetcode/21.merge-two-sorted-lists.cpp
+++ leetcode/21.merge-two-sorted-lists.cpp
@@ -1,41 +1,67 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+//     {
+//         ListNode dummy(0);
+//         ListNode *tail = &dummy;
+
+//         while (list1 && list2)
+//         {
+//             if (list1->val < list2->val)
+//             {
+//                 tail->next = list1;
+//                 list1 = list1->next;
+//             }
+//             else
+//             {
+//                 tail->next = list2;
+//                 list2 = list2->next;
+//             }
+//             tail = tail->next;
+//         }
+
+//         if (list1)
+//             tail->next = list1;
+//         if (list2)
+//             tail->next = list2;
+
+//         return dummy.next;
+//     }
+// };
+
+// 위는 반복문
+// 아래는 재귀
+
 class Solution
 {
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
     {
-        ListNode dummy(0);
-        ListNode *tail = &dummy;
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
 
-        while (list1 && list2)
+        if (list1->val < list2->val)
         {
-            if (list1->val < list2->val)
-            {
-                tail->next = list1;
-                list1 = list1->next;
-            }
-            else
-            {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
         }
-
-        if (list1)
-            tail->next = list1;
-        if (list2)
-            tail->next = list2;
-
-        return dummy.next;
+        else
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
     }
 };
