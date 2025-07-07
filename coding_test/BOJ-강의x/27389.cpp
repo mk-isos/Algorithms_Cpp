eab720a5f95beb9d367ccaea857c9ca09732d212
mk-isos <mkisos7900@naver.com>
2025-07-07 09:07:47 +0900

    250707 Create 27389.cpp

---
+++ coding_test/BOJ-강의x/27389.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define FIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(2) << n / 4.0 << '\n';
+    return 0;
+}
