b56186d40aeafe2cbc32441d1e8af93ce3f96898
Markus <mkisos7900@naver.com>
2025-10-21 09:47:43 +0900

    251021 Create 42586.cpp

---
+++ programmers/07/42586.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds)
+{
+    vector<int> answer;
+    int n = progresses.size();
+    vector<int> days_left(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        days_left[i] = ceil((100.0 - progresses[i]) / speeds[i]);
+    }
+
+    int count = 0;
+    int max_day = days_left[0];
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (days_left[i] <= max_day)
+        {
+            count++;
+        }
+        else
+        {
+            answer.push_back(count);
+            count = 1;
+            max_day = days_left[i];
+        }
+    }
+
+    answer.push_back(count);
+
+    return answer;
+}
